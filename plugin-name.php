4199b595b0467715259aa5703ca7b365db48c2a4
Pablo Hernández (OtakuPahp) <pablo@otakupahp.com>
2020-11-06 22:39:09 -0500

    Update plugin-name.php

--- plugin-name.php
+++ plugin-name.php
@@ -24,7 +24,7 @@ use Plugin_Name\Includes\Plugin_Name_Deactivator;
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name
- * Author URI:        http://example.com
+ * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
