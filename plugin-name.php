c4e3d848339c09520cd2802f5d160f179a95cdca
Pablo Hernández (OtakuPahp) <pablo@otakupahp.com>
2020-11-06 22:51:31 -0500

    Update plugin-name.php

--- plugin-name.php
+++ plugin-name.php
@@ -20,11 +20,11 @@ use Plugin_Name\Includes\Plugin_Name_Deactivator;
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin URI:        http://example.com/plugin-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name
- * Author URI:        http://example.com/
+ * Author URI:        http://author.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
