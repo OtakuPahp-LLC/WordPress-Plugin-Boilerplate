9677ddcf2cb418fd9eee2a1c42b9f012b0559483
PAHPDESKTOP\Pablo <pablo@otakupahp.com>
2020-11-06 21:55:27 -0500

    Minor changes

--- plugin-name.php
+++ plugin-name.php
@@ -19,7 +19,7 @@ namespace Plugin_Name;
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Your Name
  * Author URI:        http://example.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
