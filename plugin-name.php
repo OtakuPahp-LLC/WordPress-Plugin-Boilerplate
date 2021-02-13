2057487d9785313c9192e22652d2dc8b233bb531
Pablo Hernández (OtakuPahp) <pablo@otakupahp.com>
2021-02-13 14:51:19 -0500

    Update plugin-name.php

--- plugin-name.php
+++ plugin-name.php
@@ -50,30 +50,7 @@ define( 'PLUGIN_NAME_PLUGIN_FILE', __FILE__ );
 define( 'PLUGIN_NAME_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'PLUGIN_NAME_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-/**
- * Register autoloader
- */
-spl_autoload_register(function($required_file) {
-
-	# Transform file name from class based to file based
-	$fixed_name = strtolower( str_ireplace( '_', '-', $required_file ) );
-	$file_path = explode( '\\', $fixed_name );
-	$last_index = count( $file_path ) - 1;
-	$file_name = "class-{$file_path[$last_index]}.php";
-
-	# Get fully qualified path
-	$fully_qualified_path =  trailingslashit( dirname(__FILE__) );
-	for ( $key = 1; $key < $last_index; $key++ ) {
-		$fully_qualified_path .= trailingslashit( $file_path[ $key ] );
-	}
-	$fully_qualified_path .= $file_name;
-
-	# Include the file
-	if ( stream_resolve_include_path($fully_qualified_path) ) {
-		include_once $fully_qualified_path;
-	}
-
-});
+require_once PLUGIN_NAME_PLUGIN_PATH . 'includes/autoloader.php';
 
 /**
  * The code that runs during plugin activation.
